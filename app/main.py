1a9ad43a80c0a357973608500d1757c315f95372
Periklis Fregkos <fregkos@gmail.gr>
2024-09-18 21:16:17 +0300

    feat: Added GitHub Actions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
